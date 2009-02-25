1af5f9badb641670aa0fe8157c20b1c5b25fc75f
Andreas Sicking <outof@order.de>
2009-02-25 12:26:12 +0000

    o.O

--- db/container.php
+++ db/container.php
@@ -4,12 +4,12 @@ class DB_Container
 {
 	private $recordClass;
 	private $table;
-	
+
 	public function __construct($table, $recordClass = 'DB_Record') {
 		$this->table = $table;
 		$this->recordClass = $recordClass;
 	}
-	
+
 	// CUSTOM METHODS ----------------------------------------------------------
 	/**
 	 * @return returns only the first fitting record (or null if there is none)
@@ -17,11 +17,11 @@ class DB_Container
 	public function selectFirst($properties = '*', $condition = '', $order = '', $limit = 1) {
 		$records = $this->select($properties, $condition, $order, $limit);
 		if(count($records))
-			return $record[0];
+			return $records[0];
 		else
 			return null;
 	}
-	
+
 	/**
 	 * Abstraction for MySQL's SELECT.
 	 * @param $properties
@@ -32,7 +32,7 @@ class DB_Container
 	 */
 	public function select($properties = '*', $condition = '', $order = '', $limit = '') {
 		$records = array();
-		
+
 		$query = 'SELECT '.$properties.' FROM '.$this->table;
 		if($condition)
 			$query .= ' WHERE '.$condition;
@@ -40,9 +40,9 @@ class DB_Container
 			$query .= ' ORDER BY '.$order;
 		if($limit)
 			$query .= ' LIMIT '.$limit;
-		
+
 		$result = DB_Connection::get()->query($query);
-		
+
 		while($row = mysql_fetch_assoc($result)) {
 			$record = new $this->recordClass();
 			foreach($row as $property => $value) {
@@ -50,7 +50,7 @@ class DB_Container
 			}
 			$records[] = $record;
 		}
-		
+
 		return $records;
 	}
 }
