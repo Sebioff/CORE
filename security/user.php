ac3bde703df30cb0f6c456e2a18cfbbf2c91176c
Andreas Sicking <outof@order.de>
2009-03-06 00:18:04 +0000

    _

--- security/user.php
+++ security/user.php
@@ -1,11 +1,17 @@
 <?php
-class SECURITY_User {
+class SECURITY_User extends SECURITY_User_Dummy {
 	private $options = array();
 	public function addOptionClass(SECURITY_User_Dummy $class) {
 		$this->options[(String)$class] = $class;
+		$this->updateRights();
 	}
-	public function getOptionClasses() {
-		return $this->options;
+	public function updateRights() {
+		if (count($this->options) > 0) {
+			foreach ($this->options AS $path => $o) {
+				$this->test = $o->test;
+			}
+		} else 
+			throw Core_Exception('No Option Classes found');
 	}
 }
 ?>
