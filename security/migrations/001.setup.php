ee72ec5448e9f7f6473f0576137f5689c8de118f
Sebastian Mayer <sebioff@gmail.com>
2009-06-09 18:30:22 +0000

    - added and implemented Rakuun_Intern_Alliance_Security - implemented creating/editing alliance ranks

--- security/migrations/001.setup.php
+++ security/migrations/001.setup.php
@@ -1,40 +1,10 @@
 <?php
 
 $groupsContainer = $this->defineContainerGroups();
-$groupsUsersAssocContainer = $this->defineContainerGroupsUsersAssoc();
-$usersContainer = $this->defineContainerUsers();
-$privilegesContainer = $this->defineContainerPrivileges();
 
-$queries[] = 'CREATE TABLE IF NOT EXISTS `'.$groupsContainer[1].'` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(25) NOT NULL,
-  `group_identifier` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC AUTO_INCREMENT=0;';
-
-$queries[] = 'CREATE TABLE IF NOT EXISTS `'.$groupsUsersAssocContainer[1].'` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user` int(10) unsigned DEFAULT NULL,
-  `user_group` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user` (`user`),
-  KEY `user_group` (`user_group`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC AUTO_INCREMENT=0;';
-
-$queries[] = 'ALTER TABLE `'.$groupsUsersAssocContainer[1].'`
-  ADD CONSTRAINT `'.$groupsUsersAssocContainer[1].'_ibfk_1` FOREIGN KEY (`user`) REFERENCES `'.$usersContainer[1].'` (`id`) ON DELETE SET NULL,
-  ADD CONSTRAINT `'.$groupsUsersAssocContainer[1].'_ibfk_2` FOREIGN KEY (`user_group`) REFERENCES `'.$groupsContainer[1].'` (`id`) ON DELETE SET NULL;';
-
-$queries[] = 'CREATE TABLE IF NOT EXISTS `'.$privilegesContainer[1].'` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `privilege` varchar(255) NOT NULL,
-  `value` tinyint(1) unsigned NOT NULL,
-  `user_group` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_group` (`user_group`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC AUTO_INCREMENT=0;';
-
-$queries[] = 'ALTER TABLE `'.$privilegesContainer[1].'`
-  ADD CONSTRAINT `'.$privilegesContainer[1].'_ibfk_1` FOREIGN KEY (`user_group`) REFERENCES `'.$groupsContainer[1].'` (`id`) ON DELETE SET NULL;';
+$queries[] = 'ALTER TABLE `'.$groupsContainer[1].'`
+  ADD `alliance` smallint(5) unsigned DEFAULT NULL,
+  ADD KEY `alliance` (`alliance`),
+  ADD CONSTRAINT `'.$groupsContainer[1].'_ibfk_1` FOREIGN KEY (`alliance`) REFERENCES `alliances` (`id`) ON DELETE SET NULL;';
 
 ?>
