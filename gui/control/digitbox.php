bfa338d1d61cd41053c2cc64c469464f669936f7
Andre Jährling <dr.dent@gmx.net>
2009-08-07 19:19:37 +0000

    expand Digits Validator with min and maxValue

--- gui/control/digitbox.php
+++ gui/control/digitbox.php
@@ -6,11 +6,11 @@
  */
 class GUI_Control_DigitBox extends GUI_Control {
 	// CONSTRUCTORS ------------------------------------------------------------
-	public function __construct($name, $defaultValue = 0, $title = '') {
+	public function __construct($name, $defaultValue = 0, $title = '', $minValue = 0, $maxValue = GUI_Validator_Digits::INFINITY) {
 		parent::__construct($name, $defaultValue, $title);
 
 		$this->setTemplate(dirname(__FILE__).'/textbox.tpl');
-		$this->addValidator(new GUI_Validator_Digits());
+		$this->addValidator(new GUI_Validator_Digits($minValue, $maxValue));
 		$this->addClasses('core_gui_digitbox');
 	}
 	
--- gui/validator/digits.php
+++ gui/validator/digits.php
@@ -4,13 +4,25 @@
  * Ensures the input consists only of digits.
  */
 class GUI_Validator_Digits extends GUI_Validator {
+	const INFINITY = 999999999999999999;
+	
+	private $minValue = 0;
+	private $maxValue = self::INFINITY;
+	
 	// OVERRIDES / IMPLEMENTS --------------------------------------------------
+	public function __construct($minValue = 0, $maxValue = self::INFINITY) {
+		$this->minValue = $minValue;
+		$this->maxValue = $maxValue;
+	}
+	
 	public function isValid() {
-		return ctype_digit((string)$this->control->getValue());
+		return ctype_digit((string)$this->control->getValue()) 
+				&& $this->minValue <= $this->control->getValue()
+				&& $this->control->getValue() <= $this->maxValue;
 	}
 	
 	public function getError() {
-		return 'Darf nur Ziffern beinhalten';
+		return 'Darf nur Ziffern beinhalten und Zahl muss zwischen '.$this->minValue.' und '.$this->maxValue.' liegen';
 	}
 	
 	public function getJs() {
