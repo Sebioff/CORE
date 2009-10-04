cab3e47c674191ba3810559dbd59f3cc7198cda0
Andreas Sicking <outof@order.de>
2009-10-04 19:18:07 +0000

    some blubbs

--- gui/control/jslink.php
+++ gui/control/jslink.php
@@ -1,10 +1,14 @@
 <?php
 
+/**
+ * Control to create a link,
+ * which performs a JavaScript onclick event before entering the given url.
+ */
 class GUI_Control_JsLink extends GUI_Control_Link {
 	public function __construct($name, $caption, $js, $url = '#', $title = '') {
 		parent::__construct($name, $caption, $url, $title);
 		
-		$this->setAttribute('onclick', $js);
+		$this->setJs($js);
 	}
 	
 	public function setJs($js) {
--- gui/control/submittable.php
+++ gui/control/submittable.php
@@ -4,11 +4,6 @@
  * Base class for submittable controls
  */
 abstract class GUI_Control_Submittable extends GUI_Control {
-	// CONSTRUCTORS ------------------------------------------------------------
-	public function __construct($name, $caption = '', $title = '') {
-		parent::__construct($name, $caption, $title);
-	}
-	
 	// GETTERS / SETTERS -------------------------------------------------------
 	public function setConfirmationMessage($message) {
 		$this->setAttribute('onClick', sprintf('return(confirm(\'%s\'))', $message));
