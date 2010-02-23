430d2372a7ab1d8f99b2d8eb648713a237fcf75d
Sebastian Mayer <sebioff@gmail.com>
2010-02-23 15:30:57 +0000

    - GUI_Panel_Label now accepts panels instead of controls

--- gui/panel/label.php
+++ gui/panel/label.php
@@ -2,15 +2,15 @@
 
 class GUI_Panel_Label extends GUI_Panel {
 	// CONSTRUCTORS ------------------------------------------------------------
-	public function __construct($name, GUI_Control $control, $caption = '') {
-		if(!$caption)
-			$caption = $control->getTitle();
+	public function __construct($name, GUI_Panel $panel, $caption = '') {
+		if (!$caption)
+			$caption = $panel->getTitle();
 		parent::__construct($name, $caption);
 		
 		$this->setTemplate(dirname(__FILE__).'/label.tpl');
 		$this->addClasses('core_gui_label');
 		
-		$this->params->control = $control;
+		$this->params->panel = $panel;
 	}
 }
 
