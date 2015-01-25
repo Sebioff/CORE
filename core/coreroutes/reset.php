c9a173efe541911767c4f6efb6604ebe626f572d
Vvardenfell <Vvardenfell@users.noreply.github.com>
2015-01-25 17:25:12 +0100

    Adjusted reset callbacks functionality
    The modification allows reset callbacks to be used
    in a similar fashion as migrations.
    When a reset callback is invoked, the execution will
    be logged and consecutive executions will be suppressed.
    The callbacks are limited to one unique class/method
    combination when invoked on a particular instance of
    a given class.

--- core/coreroutes/reset.php
+++ core/coreroutes/reset.php
@@ -22,6 +22,10 @@
  * Resets the project.
  */
 class CoreRoutes_Reset extends Module {
+
+	// path of reset callback logfile, relative to PROJECT_PATH
+	const RESET_CALLBACK_LOG_FILE = '/config/log/reset_callback.log.xml';
+
 	private static $callbacksOnAfterReset = array();
 	
 	public function init() {
@@ -31,8 +35,12 @@ class CoreRoutes_Reset extends Module {
 		DB_Connection::get()->deleteTables();
 		
 		// remove migrations log
-		$file = new IO_File(PROJECT_PATH.Core_MigrationsLoader::MIGRATION_LOG_FILE);
-		$file->delete();
+		$migrationFile = new IO_File(PROJECT_PATH.Core_MigrationsLoader::MIGRATION_LOG_FILE);
+		$migrationFile->delete();
+
+		// remove reset callbacks log
+		$resetCallbacksFile = new IO_File(PROJECT_PATH.self::RESET_CALLBACK_LOG_FILE);
+		$resetCallbacksFile->delete();
 		
 		// clear global cache
 		$GLOBALS['cache']->clearAll();
@@ -46,10 +54,8 @@ class CoreRoutes_Reset extends Module {
 		// rebuild database
 		Core_MigrationsLoader::load();
 		
-		if (defined('CALLBACK_ONAFTERRESET'))
-			self::addCallbackOnAfterReset(CALLBACK_ONAFTERRESET);
-		foreach (self::$callbacksOnAfterReset as $callback)
-			call_user_func($callback);
+                // execute registered callbacks
+                self::executeCallbacksOnAfterReset();
 		
 		Scriptlet::redirect(PROJECT_ROOTURI);
 	}
@@ -57,6 +63,55 @@ class CoreRoutes_Reset extends Module {
 	public static function addCallbackOnAfterReset($callback) {
 		self::$callbacksOnAfterReset[] = $callback;
 	}
+
+	/**
+	 * Executes the callbacks registered by addCallbackOnAfterReset
+	 * or defined by the constant CALLBACK_ONAFTERRESET.
+	 * Execution is only performed if the callbacks haven't been called
+	 * since the last reset.
+	 * This method is intended to be used when there's a reason to execute
+	 * these callbacks once, even when there's no complete reset being performed.
+	 * One such reason is the initial setup and running newly added callbacks
+	 * in development mode.
+	 * Callbacks that differ only in the instance of a class and not in the method called
+	 * will be treated as if they were the same. So if more than one instance of a class
+	 * is used for a callback, each instance needs its unique member function to be executed.
+	 */
+	public static function executeCallbacksOnAfterReset() {
+
+		if (defined('CALLBACK_ONAFTERRESET'))
+			self::addCallbackOnAfterReset(CALLBACK_ONAFTERRESET);
+
+		if (file_exists(PROJECT_PATH.self::RESET_CALLBACK_LOG_FILE))
+			$xml = simplexml_load_file(PROJECT_PATH.self::RESET_CALLBACK_LOG_FILE);
+		else {
+			$baseResetCallbackLog = '<?xml version=\'1.0\'?><content></content>';
+			$xml = new SimpleXMLElement($baseResetCallbackLog);
+		}
+
+		foreach (self::$callbacksOnAfterReset as $callback) {
+			if (is_array($callback) && isset($callback[0]) && isset($callback[1])) {
+				// different objects of the same class will be treated as if they were the same
+				$callbackIdentifier = get_class($callback[0]).'->'.$callback[1];
+			} else $callbackIdentifier = $callback;
+
+			// execute reset callbacks if they haven't been before
+			$result = $xml->xpath(sprintf('/content/callback[@identifier=\'%s\']', $callbackIdentifier));
+			if (empty($result)) {
+				$result = $xml->xpath('/content');
+				$child = $result[0]->addChild('callback');
+				$child->addAttribute('identifier', $callbackIdentifier);
+				call_user_func($callback);
+                        }
+                }
+		// save reset callback logfile
+		$doc = new DOMDocument('1.0');
+		$doc->preserveWhiteSpace = false;
+		$doc->loadXML($xml->asXML());
+		$doc->formatOutput = true;
+		$doc->save(PROJECT_PATH.self::RESET_CALLBACK_LOG_FILE);
+        }
+
 }
 
 ?>
